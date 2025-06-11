5fae970f329e6529f333e8a493d125248f7cd038
suu2626 <suu2626@gmail.com>
2025-06-11 16:39:46 +0900

    update_App.js_index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // ver____App.js
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 
