3b429b6cd0fe2da0591b54eadfac825121a3686f
suu2626 <suu2626@gmail.com>
2025-06-17 13:35:43 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 
